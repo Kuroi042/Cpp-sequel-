5df5810598bc815c9da23ee75876b0f0ee99b037
Mohamed charaf Bouderraoui <mbouderr@c1r4p9.1337.ma>
2024-07-01 04:20:49 +0100

    alright parss is done

--- cpp06/ScalarConverte.cpp
+++ cpp06/ScalarConverte.cpp
@@ -9,56 +9,41 @@ private:
 public:
         static void convert(std::string raw)
         {
-
-            
-                 if (checkdigit(raw) == 2)
-                        printf("all digit\n");
-                else if (checkdigit(raw) == 1)
-                        printf("all alpha\n");
-                        else 
-                        printf("errpr\n");
+        if(checkRaw(raw) == 1)
+                printf("is digit\n");
+        else if(checkRaw(raw) == 2)
+                printf("is alpha");
+        else 
+                printf("error\n");
         }
-        static int checkdigit(std::string raw)
+        static int checkRaw(std::string raw)
         {
                 int i = 0;
                 bool digit = 0;
                 bool alpha = 0;
-                // case all digit
                 while (raw[i] != '\0')
                 {
                         if (isdigit(raw[i]))
-                        {
                                 digit = 1;
-                        }
                         else
                                 break;
                         i++;
                 }
+                if (raw[i] == '\0' && digit)
+                        return 1;
+
                 i = 0;
                 while (raw[i] != '\0')
                 {
                         if (isalpha(raw[i]))
-                        {
                                 alpha = 1;
-                        }
                         else
                                 break;
                         i++;
                 }
-
-                i = 0;
-
-                if (raw[i] == '\0')
-                {
-                        if (alpha == 1)
-                                return 1;
-                        if (digit == 1)
-                                return 2;
-                }
-                else if (raw[i] != '\0')
-                        return 3;
-
-                return 0;
+                if (raw[i] == '\0' && alpha)
+                        return 2;
+                return 3;
         }
 };
 
@@ -74,24 +59,4 @@ int main(int argc, char **argv)
                 std::cout << "argument are incorrect\n";
         return (EXIT_FAILURE);
 }
-
-// for (int i = 0; i < (int)raw.length(); i++)
-// {
-//         for(int j = 0;j<i;j++){
-//         if (isdigit(raw[j]) && is_alpha != 1)
-//         {
-//                 is_digit = 1;
-//                 return 1;
-//         }
-//         else if (isalpha(raw[j])&& is_digit != 1)
-//         {
-//                 is_alpha = 1;
-//                 return 2;
-//         }
-
-//         else {
-//         printf("makayn ta9lwa\n");
-//                 return 3;
-//         }
-//         }
-// }
+ 
