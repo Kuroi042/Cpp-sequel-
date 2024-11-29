d7d43ebdc6ab4d48d534ab5ef65e7a5acaac7f2d
Mohamed charaf Bouderraoui <mbouderr@c1r3p5.1337.ma>
2024-11-29 01:06:09 +0100

    oki

--- cpp09/ex00/BitcoinExchange.hpp
+++ cpp09/ex00/BitcoinExchange.hpp
@@ -16,8 +16,6 @@ class Bitcoin
     public:
          Bitcoin(){};
         Bitcoin( char *argv );
-                   
-           
         ~Bitcoin(){}
  
           map<string,float>getMap()
@@ -28,3 +26,32 @@ void     parse_datacsv(ifstream &file);
 void  openfile(const char* argv, const std::string& fileName);
 
 };
+
+      //  while (getline(file, line))
+      //   {
+      //       size_t dilimiter = line.find('-');
+      //       if(dilimiter!= line.npos){
+      //       year =  (line.substr(0, dilimiter) );
+      //               if(year.length()>4 ){
+      //                   cout<<"years kbir\n";
+                        
+      //               }
+      //               cout<<"years == "<<year<<std::endl;
+      //           //    break;
+      //                   cout<<"dilimiter == "<<dilimiter<<std::endl;      
+      //               month  = (line.substr(dilimiter+1, dilimiter) );
+      //               cout<<"month  = "<<month <<std::endl;
+      //                   cout<<"dilimiter == "<<dilimiter<<std::endl;      
+      //               dilimiter+=2;
+      //               day   = (line.substr(dilimiter+1, line.find('|') ) );
+      //                 cout<<"day= "<<day <<std::endl;
+      //       }
+ 
+    //  string dilimiter = "-";
+    //             string value =  line.substr(0, line.find('-')) ;
+    //               year = static_cast<int>(atoi(value.c_str()));                                                    
+    //                 cout<<"years "<< year <<std::endl;
+    //                         // value.empty();
+    //                 string month=  line.substr(value.append(dilimiter).length(), value );
+    //                             cout<<"month == |"<<month<<"|"<<std::endl;
+                    
--- cpp09/ex00/test.cpp
+++ cpp09/ex00/test.cpp
@@ -1,7 +1,10 @@
 #include <iostream>
 #include <fstream>
 #include <map>
+ #include <cstdlib>
 #include <string>
+
+
 using namespace std;
 
 int main(int argc, char **argv)
@@ -14,30 +17,25 @@ int main(int argc, char **argv)
             cout << "ERROR OPEN\n";
             exit(1);
         }
-        string line;
+        string key;
+        string value;
     string year;
-    string month;
     string day;
-        while (getline(file, line))
-        {
-            size_t dilimiter = line.find('-');
-            if(dilimiter!= line.npos){
-            year =  (line.substr(0, dilimiter) );
-            cout << "years == |" << year <<"|"<< std::endl;
-            month = (line.substr(year.length()+1, dilimiter-2) );
+        getline(file,key, '|');
+        getline( file,value  );
+                cout<<key<<endl;
+                cout<<value<<endl;
+        size_t year_start  = value.find_first_not_of(" ");
+        size_t year_end =  value.find_last_not_of(" ");
+        
+            value =  value.substr(year_start, year_end-year_start+1);
+                cout<<value<<std::endl;
+         
 
-            cout << "month == |" << month <<"|"<< std::endl;
-            dilimiter++;
-            cout<<dilimiter<<endl;
-            day =  (line.substr(line.npos, line.find('|') ));
-                cout<<"day  == |"<<day<<"|"<<std::endl;;
-
-            }
-
-
-        }
-        file.close();
-    }
+   
+        }           
 else
     cout << "argc is incorrect\n";
-}
+    }
+    
+ 
